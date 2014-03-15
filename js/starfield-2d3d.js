a3083442cf648f90478d9794bdf0becd0bfa5cef
Jan Horava <czechinthemail@gmail.com>
2014-03-15 14:59:49 -0700

    removed get() function, improved formatting

--- js/starfield-2d3d.js
+++ js/starfield-2d3d.js
@@ -1,62 +1,62 @@
 function draw(){
     canvas.clearRect(
-        0,
-        0,
-        width,
-        height
+      0,
+      0,
+      width,
+      height
     );
 
     // create 5 stars at random positions
     i = 4;
     do{
         stars.push([
-            Math.random() * width,// star x
-            Math.random() * height,// star y
-            0// star brightness
+          Math.random() * width,// star x
+          Math.random() * height,// star y
+          0// star brightness
         ]);
     }while(i--);
 
     i = stars.length - 1;
     do{
-        if(stars[i][0] < 0 || stars[i][0] > width || stars[i][1] < 0 || stars[i][1] > height){
+        if(stars[i][0] < 0
+          || stars[i][0] > width
+          || stars[i][1] < 0
+          || stars[i][1] > height){
             // splice stars that are outside the canvas boundaries
-            stars.splice(
-                i,
-                1
-            );
+            stars.splice(i, 1);
 
         }else{
             // increase star brightness
             stars[i][2] += 9;
 
             // update star positions based on brightness
-            stars[i][0] += Math.abs((stars[i][0] - x) / x) * ((stars[i][0] > x ? ratio : -ratio) * 9) * (stars[i][2] / 99);
-            stars[i][1] += Math.abs((stars[i][1] - y) / y) * (stars[i][1] > y ? 9 : -9) * (stars[i][2] / 99);
+            stars[i][0] += Math.abs((stars[i][0] - x) / x)
+              * ((stars[i][0] > x ? ratio : -ratio) * 9)
+              * (stars[i][2] / 99);
+            stars[i][1] += Math.abs((stars[i][1] - y) / y)
+              * (stars[i][1] > y ? 9 : -9)
+              * (stars[i][2] / 99);
 
             // draw stars
             canvas.fillStyle = 'rgb(' + stars[i][2] + ', '
-                                      + stars[i][2] + ', '
-                                      + stars[i][2] + ')';
+              + stars[i][2] + ', '
+              + stars[i][2] + ')';
             canvas.fillRect(
-                stars[i][0],
-                stars[i][1],
-                3,
-                3
+              stars[i][0],
+              stars[i][1],
+              3,
+              3
             );
         }
     }while(i--);
 }
 
-function get(i){
-    return document.getElementById(i);
-}
-
 function resize(){
     width = window.innerWidth;
-    get('canvas').width = width;
+    document.getElementById('canvas').width = width;
 
     height = window.innerHeight;
-    get('canvas').height = height;
+    document.getElementById('canvas').height = height;
 
     x = width / 2;
     y = height / 2;
@@ -64,7 +64,7 @@ function resize(){
     ratio = width / height;
 }
 
-var canvas = get('canvas').getContext('2d');
+var canvas = document.getElementById('canvas').getContext('2d');
 var height = 0;
 var i = 0;
 var ratio = 0;
