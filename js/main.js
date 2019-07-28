f30323e16ac86c4b7e6224efb6b54afee6526679
Jan Hořava <czechinthemail@gmail.com>
2019-07-28 16:47:58 -0700

    updated entity usage

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 function draw_logic(){
     // Draw stars.
-    core_group_modify({
+    entity_group_modify({
       'groups': [
         'star',
       ],
@@ -10,14 +10,14 @@ function draw_logic(){
           canvas_setproperties({
             'properties': {
               'fillStyle': 'rgb('
-                + core_entities[entity]['brightness'] + ', '
-                + core_entities[entity]['brightness'] + ', '
-                + core_entities[entity]['brightness'] + ')',
+                + entity_entities[entity]['brightness'] + ', '
+                + entity_entities[entity]['brightness'] + ', '
+                + entity_entities[entity]['brightness'] + ')',
             },
           });
           canvas_buffer.fillRect(
-            core_entities[entity]['x'],
-            core_entities[entity]['y'],
+            entity_entities[entity]['x'],
+            entity_entities[entity]['y'],
             3,
             3
           );
@@ -29,7 +29,7 @@ function logic(){
     // Create 5 stars at random positions.
     let loop_counter = 4;
     do{
-        core_entity_create({
+        entity_create({
           'properties': {
             'x': core_random_integer({
               'max': canvas_properties['width'],
@@ -44,17 +44,17 @@ function logic(){
         });
     }while(loop_counter--);
 
-    core_group_modify({
+    entity_group_modify({
       'groups': [
         'star',
       ],
       'todo': function(entity){
           // Delete stars that are outside the canvas boundaries.
-          if(core_entities[entity]['x'] < 0
-            || core_entities[entity]['x'] > canvas_properties['width']
-            || core_entities[entity]['y'] < 0
-            || core_entities[entity]['y'] > canvas_properties['height']){
-              core_entity_remove({
+          if(entity_entities[entity]['x'] < 0
+            || entity_entities[entity]['x'] > canvas_properties['width']
+            || entity_entities[entity]['y'] < 0
+            || entity_entities[entity]['y'] > canvas_properties['height']){
+              entity_remove({
                 'entities': [
                   entity,
                 ],
@@ -63,32 +63,31 @@ function logic(){
           }
 
           // Increase star brightness.
-          core_entities[entity]['brightness'] += 9;
+          entity_entities[entity]['brightness'] += 9;
 
           // Update star positions based on brightness.
-          core_entities[entity]['x'] += Math.abs((core_entities[entity]['x'] - canvas_properties['width-half']) / canvas_properties['width-half'])
-            * ((core_entities[entity]['x'] > canvas_properties['width-half'] ? ratio : -ratio) * 9)
-            * (core_entities[entity]['brightness'] / 99);
-          core_entities[entity]['y'] += Math.abs((core_entities[entity]['y'] - canvas_properties['height-half']) / canvas_properties['height-half'])
-            * (core_entities[entity]['y'] > canvas_properties['height-half'] ? 9 : -9)
-            * (core_entities[entity]['brightness'] / 99);
+          entity_entities[entity]['x'] += Math.abs((entity_entities[entity]['x'] - canvas_properties['width-half']) / canvas_properties['width-half'])
+            * ((entity_entities[entity]['x'] > canvas_properties['width-half'] ? ratio : -ratio) * 9)
+            * (entity_entities[entity]['brightness'] / 99);
+          entity_entities[entity]['y'] += Math.abs((entity_entities[entity]['y'] - canvas_properties['height-half']) / canvas_properties['height-half'])
+            * (entity_entities[entity]['y'] > canvas_properties['height-half'] ? 9 : -9)
+            * (entity_entities[entity]['brightness'] / 99);
       },
     });
 }
 
 function repo_init(){
     core_repo_init({
-      'entities': {
-        'star': {
-          'properties': {
-            'brightness': 0,
-          },
-        },
-      },
       'globals': {
         'ratio': 0,
       },
       'title': 'Starfield-2D3D.htm',
+    });
+    entity_set({
+      'properties': {
+        'brightness': 0,
+      },
+      'type': 'star',
     });
     canvas_init();
 }
