d3933f34f9ddda7b7894890c6f9f5ae7c8b3d060
Jan Hořava <czechinthemail@gmail.com>
2025-06-16 03:15:36 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -8,15 +8,15 @@ function repo_drawlogic(){
       'todo': function(entity){
           canvas_setproperties({
             'fillStyle': 'rgb('
-              + entity['brightness'] + ', '
-              + entity['brightness'] + ', '
-              + entity['brightness'] + ')',
+              + entity.brightness + ', '
+              + entity.brightness + ', '
+              + entity.brightness + ')',
           });
           canvas.fillRect(
-            entity['x'],
-            entity['y'],
-            core_storage_data['stars-width'],
-            core_storage_data['stars-height']
+            entity.x,
+            entity.y,
+            core_storage_data.stars_width,
+            core_storage_data.stars_height
           );
       },
     });
@@ -28,13 +28,13 @@ function repo_init(){
         'ratio': 0,
       },
       'storage': {
-        'stars-frame': 4,
-        'stars-height': 3,
-        'stars-width': 3,
+        'stars_frame': 4,
+        'stars_height': 3,
+        'stars_width': 3,
       },
-      'storage-menu': '<table><tr><td><input class=mini id=stars-frame min=1 step=1 type=number><td>Stars/Frame'
-        + '<tr><td><input class=mini id=stars-height min=1 step=any type=number><td>Stars Height'
-        + '<tr><td><input class=mini id=stars-width min=1 step=any type=number><td>Stars Width</table>',
+      'storage-menu': '<table><tr><td><input class=mini id=stars_frame min=1 step=1 type=number><td>Stars/Frame'
+        + '<tr><td><input class=mini id=stars_height min=1 step=any type=number><td>Stars Height'
+        + '<tr><td><input class=mini id=stars_width min=1 step=any type=number><td>Stars Width</table>',
       'title': 'Starfield-2D3D.htm',
     });
     entity_set({
@@ -47,12 +47,12 @@ function repo_init(){
 }
 
 function repo_logic(){
-    let loop_counter = Math.floor(core_storage_data['stars-frame']);
+    let loop_counter = Math.floor(core_storage_data.stars_frame);
     do{
         entity_create({
           'properties': {
-            'x': core_random_integer(canvas_properties['width']),
-            'y': core_random_integer(canvas_properties['height']),
+            'x': core_random_integer(canvas_properties.width),
+            'y': core_random_integer(canvas_properties.height),
           },
           'types': [
             'star',
@@ -65,29 +65,29 @@ function repo_logic(){
         'star',
       ],
       'todo': function(entity){
-          if(entity['x'] < 0
-            || entity['x'] > canvas_properties['width']
-            || entity['y'] < 0
-            || entity['y'] > canvas_properties['height']){
+          if(entity.x < 0
+            || entity.x > canvas_properties.width
+            || entity.y < 0
+            || entity.y > canvas_properties.height){
               entity_remove({
                 'entities': [
-                  entity['id'],
+                  entity.id,
                 ],
               });
               return;
           }
 
-          entity['brightness'] += 9;
-          entity['x'] += Math.abs((entity['x'] - canvas_properties['width-half']) / canvas_properties['width-half'])
-            * ((entity['x'] > canvas_properties['width-half'] ? ratio : -ratio) * 9)
-            * (entity['brightness'] / 99);
-          entity['y'] += Math.abs((entity['y'] - canvas_properties['height-half']) / canvas_properties['height-half'])
-            * (entity['y'] > canvas_properties['height-half'] ? 9 : -9)
-            * (entity['brightness'] / 99);
+          entity.brightness += 9;
+          entity.x += Math.abs((entity.x - canvas_properties.width_half) / canvas_properties.width_half)
+            * ((entity.x > canvas_properties.width_half ? ratio : -ratio) * 9)
+            * (entity.brightness / 99);
+          entity.y += Math.abs((entity.y - canvas_properties.height_half) / canvas_properties.height_half)
+            * (entity.y > canvas_properties.height_half ? 9 : -9)
+            * (entity.brightness / 99);
       },
     });
 }
 
 function repo_resizelogic(){
-    ratio = canvas_properties['width'] / canvas_properties['height'];
+    ratio = canvas_properties.width / canvas_properties.height;
 }
